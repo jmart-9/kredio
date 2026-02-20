cd0f331ebc2d8a379c2ab71e04417e3801528a5e
REDDCOM S.A de C.V <jersonmart9@gmail.com>
2026-02-20 16:47:57 -0600

    V0.51 actualiación nombre de clase principal

--- src/main/java/com/kredio/KredioApplication.java
+++ src/main/java/com/kredio/KredioApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CopresasApplication {
+public class KredioApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CopresasApplication.class, args);
+        SpringApplication.run(KredioApplication.class, args);
     }
 }
